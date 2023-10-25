b60a37f445f4d9bef6a02c9f95aa0eec8ef967d7
Nichole Guan <nicholeguan2@gmail.com>
2023-10-24 20:36:50 -0700

    add Rescue fixtures and add more unit tests for Rescue model

--- server/test/models/rescue.js
+++ server/test/models/rescue.js
@@ -8,11 +8,11 @@ import models from '../../models/index.js';
 
 describe('models.Rescue', () => {
     beforeEach(async () => {
-        await helper.loadFixtures([]);
+        await helper.loadFixtures(['rescues']);
     });
 
     it('creates a new Rescue record', async () => {
-        assert.deepStrictEqual(await models.Rescue.count(),0);
+        assert.deepStrictEqual(await models.Rescue.count(), 3);
         const record = await models.Rescue.create({
             Name: 'Test Rescue',
             Address: '123 Street, City, CA 12345',
@@ -24,8 +24,29 @@ describe('models.Rescue', () => {
             Logo: 'https://image.com',
             Animals: 'animal1, animal2'
         });
-        assert.deepStrictEqual(await models.Rescue.count(), 1);
+        assert.deepStrictEqual(await models.Rescue.count(), 4);
         assert.notDeepStrictEqual(record.id, null);
         assert.deepStrictEqual(record.Name, 'Test Rescue');
     });
+
+    it('finds a Rescue record by its id', async () => {
+        const record = await models.Rescue.findByPk(1001);
+        assert.notDeepStrictEqual(record, null);
+        assert.deepStrictEqual(record.Name, 'Fixture Rescue 1001');
+    });
+
+    it('finds multiple Rescue records', async () => {
+        const records = await models.Rescue.findAll({
+            order: [['Name','ASC']]
+        });
+        assert.deepStrictEqual(records.length, 3);
+        assert.deepStrictEqual(records[0].Name, 'Fixture Rescue 1000');
+    });
+
+    it('deletes a Rescue record', async () => {
+        assert.deepStrictEqual(await models.Rescue.count(), 3);
+        const record = await models.Rescue.findByPk(1001);
+        await record.destroy();
+        assert.deepStrictEqual(await models.Rescue.count(), 2);
+    })
 });
