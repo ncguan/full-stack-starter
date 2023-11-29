6a0d34318d27216b5e77c681c5a3dd338da014e5
Nichole Guan <nicholeguan2@gmail.com>
2023-11-28 19:34:11 -0800

    finish CRUD api endpoints for rescues

--- server/routes/api/rescues.js
+++ server/routes/api/rescues.js
@@ -8,10 +8,22 @@ import helpers from '../helpers.js';
 
 const router = express.Router();
 
+router.post('/', async (req, res) => {
+    try {
+        const record = await models.Rescue.create(_.pick(req.body, [
+            'Name', 'Address', 'Website', 'Email', 'Phone', 'Services', 'Fee', 'Logo', 'Animals'
+        ]));
+        res.status(StatusCodes.CREATED).json(record.toJSON());
+    } catch (err) {
+        console.log(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+    }
+});
+
 router.get('/', async (req, res) => {
     const records = await models.Rescue.findAll();
     res.json(records.map((record) => record.toJSON()))
-})
+});
 
 router.get('/:id', async (req, res) => {
     try {
@@ -19,7 +31,32 @@ router.get('/:id', async (req, res) => {
         res.json(record.toJSON());
     } catch (err) {
         console.log(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
     }
-})
+});
+
+router.patch('/:id', async (req, res) => {
+    try {
+        const record = await models.Rescue.findByPk(req.params.id);
+        await record.update(_.pick(req.body, [
+            'Name', 'Address', 'Website', 'Email', 'Phone', 'Services', 'Fee', 'Logo', 'Animals'
+        ]));
+        res.json(record.toJSON());
+    } catch (err) {
+        console.log(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const record = await models.Rescue.findByPk(req.params.id);
+        await record.destroy();
+        res.status(StatusCodes.OK).end();
+    } catch (err) {
+        console.log(err);
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+    }
+});
 
 export default router;
--- server/test/routes/api/rescues.js
+++ server/test/routes/api/rescues.js
@@ -5,6 +5,7 @@ import session from 'supertest-session';
 
 import helper from '../../helper.js';
 import app from '../../../app.js';
+import models from '../../../models/index.js'
 
 describe('/api/rescues', () => {
   let testSession;
@@ -12,6 +13,16 @@ describe('/api/rescues', () => {
   beforeEach(async () => {
     await helper.loadFixtures(['rescues']);
     testSession = session(app);
+  });
+
+  it('creates a new Rescue', async () => {
+    const response = await testSession.post('/api/rescues')
+    .send({Name: 'Created Name', Address: 'Created Address', Website: 'Created Website', Email: 'Created Email', Phone: 'Created Phone', Services: 'Ceated Services', Fee:'Created Fee', Logo: 'Created Logo', Animals: 'Created Animals'})
+    .expect(StatusCodes.CREATED);
+
+    const record = await models.Rescue.findByPk(response.body.id);
+    assert.deepStrictEqual(record.Name, 'Created Name');
+    assert.deepStrictEqual(record.Logo, 'Created Logo')
   });
 
   it('fetch all rescues from the Rescues table', async () => {
@@ -22,5 +33,18 @@ describe('/api/rescues', () => {
   it('fetch one rescue record from the Rescue table', async () => {
     const response = await testSession.get('/api/rescues/1001').expect(StatusCodes.OK);
     assert.deepStrictEqual(response.body?.Name, 'Fixture Rescue 1001');
+  });
+
+  it('updates an existing Rescue', async () => {
+    await testSession.patch('/api/rescues/1001')
+    .send({Name: 'Updated Name'})
+    .expect(StatusCodes.OK)
+  });
+
+  it('deletes an existing Rescue', async () => {
+    await testSession.delete('/api/rescues/1001').expect (StatusCodes.OK);
+
+    const record = await models.Rescue.findByPk(1001);
+    assert.deepStrictEqual(record, null);
   })
 })
